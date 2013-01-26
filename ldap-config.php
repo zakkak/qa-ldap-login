791b5d57cf92f6ee93a27d1ae56319919aa8ce71
zakkak <foivos@zakkak.net>
2013-01-26 03:38:19 +0200

    Adding missing ;

--- ldap-config.php
+++ ldap-config.php
@@ -1,6 +1,6 @@
 <?php
 
-$ldap_search_strings = array( 'uid=USERNAME,ou=lala,ou=www,dc=example,dc=com', 'uid=USERNAME,ou=lala2,ou=www,dc=example,dc=com' )
+$ldap_search_strings = array( 'uid=USERNAME,ou=lala,ou=www,dc=example,dc=com', 'uid=USERNAME,ou=lala2,ou=www,dc=example,dc=com' );
 $ldap_hostname = 'ldaps://ldap.example.com'; // use ldap:// for non ssl encrypted servers
 $ldap_port = 636; // use 389 for non ssl encrypted servers
 
