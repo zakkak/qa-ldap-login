9071e86c2c68ee18614842d6ddde1bc0cafb7ba3
Joustie <joustie@gmail.com>
2013-04-11 17:19:46 +0300

    Update ldap-config.php
    Added extra items needed to use Active Directory

--- ldap-config.php
+++ ldap-config.php
@@ -1,11 +1,17 @@
 <?php
 
-$ldap_search_strings = array( 'uid=USERNAME,ou=lala,ou=www,dc=example,dc=com', 'uid=USERNAME,ou=lala2,ou=www,dc=example,dc=com' );
-$ldap_hostname = 'ldaps://ldap.example.com'; // use ldap:// for non ssl encrypted servers
-$ldap_port = 636; // use 389 for non ssl encrypted servers
-$ldap_filter = '(|(objectClass=user)(objectClass=person)(objectClass=employee))';
+// This account must have read acces to the AD
+// Can also be in the form of the UPN, like TEST@TEST.local
+$ldap_service_account_bind = "CN=TEST,CN=Service Accounts,DC=TEST,DC=local";
+$ldap_service_account_pwd = "PASS";
+//The search for the use will take place in the following context, change it to yours
+$user_dn = "OU=SBSUsers,OU=Users,OU=MyBusiness,DC=TEST,DC=local";
+$ldap_userfield = "sAMAccountName";
+$ldap_hostname = 'ldap://192.168.1.1'; // use ldap:// for non ssl encrypted servers
+$ldap_port = 389; // use 389 for non ssl encrypted servers
+$ldap_filter = '|(objectClass=*)';
 $ldap_fname = 'givenname';
 $ldap_sname = 'sn';
-$ldap_mail = 'email';
+$ldap_mail = 'mail';
 
 ?>
