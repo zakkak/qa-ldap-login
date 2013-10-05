7733a29bf441a88695d224126f0427c6e9c1ec4b
Foivos <foivos@zakkak.net>
2013-10-05 08:20:59 +0300

    Fixing info and adding links to github in qa-plugin.php

--- qa-plugin.php
+++ qa-plugin.php
@@ -3,9 +3,12 @@
 /*
   Plugin Name: LDAP-Login
   Plugin Description: Allows ldap authentication within Q2A
-  Plugin Version: 0.3
-  Plugin Date: 2013-01-25
-  Plugin Authors: Karl Bitz, Foivos Zakkak
+  Plugin URI: https://github.com/zakkak/qa-ldap-login
+  Plugin Update Check URI: https://github.com/zakkak/qa-ldap-login/raw/master/qa-plugin.php
+  Plugin Version: 0.4
+  Plugin Date: 2013-10-04
+  Plugin Authors: Karl Bitz, Foivos S. Zakkak
+  Plugin Author URI: foivos.zakkak.net
   Plugin License: Free
   Plugin Minimum Question2Answer Version: 1.4
 */
