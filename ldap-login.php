8fb0abc444107c465a6a63a9656e41eeb3b629c9
Foivos S. Zakkak <foivos@zakkak.net>
2013-10-05 18:14:00 +0300

    Merging sweetlilmre's changes

--- ldap-login.php
+++ ldap-login.php
@@ -1,58 +1,52 @@
 <?php
 
 class ldap_login {
+  function load_module($directory, $urltoroot) {
+    $this->directory=$directory;
+    $this->urltoroot=$urltoroot;
+  } // end function load_module
 
-	var $directory;
-	var $urltoroot;
+  // check_login checks to see if user is already logged in by looking for
+  // a cookie or session variable (dependent on 'remember me' setting
+  function check_login() {
+    if(!isset($_COOKIE["qa-login_fname"]) && !isset($_SESSION["qa-login_fname"])) {
+      return false;
+    } else {
+      if(isset($_COOKIE["bdops-login_fname"])) {
+        $fname = $_COOKIE["qa-login_fname"];
+        $lname = $_COOKIE["qa-login_lname"];
+        $email = $_COOKIE["qa-login_email"];
+        $username = $_COOKIE["qa-login_user"];
+      } else {
+        $fname = $_SESSION["qa-login_fname"];
+        $lname = $_SESSION["qa-login_lname"];
+        $email = $_SESSION["qa-login_email"];
+        $username = $_SESSION["qa-login_user"];
+      }
+      $source = 'ldap';
+      $identifier = $email;
 
-	function load_module($directory, $urltoroot) {
-		$this->directory=$directory;
-		$this->urltoroot=$urltoroot;
-	} // end function load_module
+      $fields['email'] = $email;
+      $fields['confirmed'] = true;
+      $fields['handle'] = $username;
+      $fields['name'] = $fname . " " . $lname;
+      qa_log_in_external_user($source,$identifier,$fields);
+    }
+  } // end function check_login
 
-	// check_login checks to see if user is already logged in by looking for
-	// a cookie or session variable (dependent on 'remember me' setting
-	function check_login() {
+  function match_source($source) {
+    return $source=='ldap';
+  }
 
-		if(!isset($_COOKIE["qa-login_fname"])
-		   && !isset($_SESSION["qa-login_fname"])) {
-			return false;
-		} else {
-			if(isset($_COOKIE["bdops-login_fname"])){
-				$fname    = $_COOKIE["qa-login_fname"];
-				$lname    = $_COOKIE["qa-login_lname"];
-				$email    = $_COOKIE["qa-login_email"];
-				$username = $_COOKIE["qa-login_user"];
-			} else {
-				$fname    = $_SESSION["qa-login_fname"];
-				$lname    = $_SESSION["qa-login_lname"];
-				$email    = $_SESSION["qa-login_email"];
-				$username = $_SESSION["qa-login_user"];
-			}
-			$source              = 'ldap';
-			$identifier          = $email;
+  function login_html($tourl, $context) {} 
 
-			$fields['email']     = $email;
-			$fields['confirmed'] = true;
-			$fields['handle']    = $username;
-			$fields['name']      = $fname . " " . $lname;
-			qa_log_in_external_user($source,$identifier,$fields);
-		}
-	} // end function check_login
+  function logout_html ($tourl) {
+    require_once QA_INCLUDE_DIR."qa-base.php";
 
-	function match_source($source) {
-		return $source=='ldap';
-	} // end function match_source
-
-	function login_html($tourl, $context) {}
-
-	function logout_html ($tourl) {
-		require_once QA_INCLUDE_DIR."qa-base.php";
-
-		$_SESSION['logout_url'] = $tourl;
-		$logout_url = qa_path('auth/logout', null, qa_path_to_root());
-		echo('<a href="'.$logout_url.'">'.qa_lang_html('main/nav_logout').'</a>');
-	} // end function logout_html
+    $_SESSION['logout_url'] = $tourl;
+    $logout_url = qa_path('auth/logout', null, qa_path_to_root());
+    echo('<a href="'.$logout_url.'">'.qa_lang_html('main/nav_logout').'</a>');
+  } // end function logout_html
 
 } // end class ldap_login
 
--- qa-plugin.php
+++ qa-plugin.php
@@ -6,9 +6,8 @@
   Plugin URI: https://github.com/zakkak/qa-ldap-login
   Plugin Update Check URI: https://github.com/zakkak/qa-ldap-login/raw/master/qa-plugin.php
   Plugin Version: 0.4
-  Plugin Date: 2013-10-04
-  Plugin Authors: Karl Bitz, Foivos S. Zakkak
-  Plugin Author URI: foivos.zakkak.net
+  Plugin Date: 2013-10-05
+  Plugin Author: Karl Bitz, Foivos S. Zakkak, Peter Edwards
   Plugin License: Free
   Plugin Minimum Question2Answer Version: 1.4
 */
@@ -24,6 +23,7 @@ if (!defined('QA_VERSION')) {
 qa_register_plugin_module('login','ldap-login.php','ldap_login','LDAP Login');
 qa_register_plugin_layer('ldap-login-layer.php','LDAP Login Layer');
 qa_register_plugin_module('page','ldap-login-logout-page.php','ldap_logout_process','LDAP Logout Process');
+qa_register_plugin_module('module', 'ldap-login-admin-form.php', 'ldap_login_admin_form', 'LDAP Login');
 
 /*
   Omit PHP closing tag to help avoid accidental output
