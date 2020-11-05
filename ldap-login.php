857ad8bf3ca891cb33fa6e8816ed0d3e198c1afc
Remy Blom <remy.blom@hku.nl>
2020-11-05 08:35:18 +0000

    removed unnecessary comments

--- ldap-login.php
+++ ldap-login.php
@@ -4,17 +4,10 @@ class ldap_login {
   function load_module($directory, $urltoroot) {
     $this->directory=$directory;
     $this->urltoroot=$urltoroot;
-  } // end function load_module
-
+  }
   function match_source($source) {
     return $source=='ldap';
   }
-
-  /*
-  REMY BLOM REMOVED ALL OTHER FUNCTIONS FROM THIS CLASS
-  Their functionality is already provided by q2a-core...
-  */
-
-} // end class ldap_login
+}
 
 ?>
